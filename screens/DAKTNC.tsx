f911905a2d6cd4dad9e6b0e4abb3a06b2c36960a
ThanhBui25 <Buihuuthanh2510@gmail.com>
2024-11-03 14:37:56 +0700

    main

--- screens/DAKTNC.tsx
+++ screens/DAKTNC.tsx
@@ -1,79 +1,39 @@
 import React, { useEffect, useState } from 'react';
 import { SafeAreaView, Text, ScrollView, View, TouchableOpacity, StyleSheet, ActivityIndicator } from 'react-native';
-import firestore from '@react-native-firebase/firestore';
+import database from '@react-native-firebase/database';
 
 const DAKTNC: React.FC<{ navigation: any, route: any }> = ({ navigation, route }) => {
   const [daktnc, setDaktnc] = useState<any[]>([]);
   const [activeButton, setActiveButton] = useState<string>('DHDTMT17B');
   const [loading, setLoading] = useState<boolean>(true);
 
-  const fetchDaktncFromFirestore = async () => {
+  const fetchDaktncFromRealtimeDatabase = async () => {
     setLoading(true);
     try {
-      const snapshot = await firestore()
-        .collection('daktnc')
-        .orderBy('TimeInOut', 'asc') // Sắp xếp theo TimeInOut từ sớm đến trễ
-        .get();
-
+      const snapshot = await database()
+        .ref('rooms/room1/peopleInRoom')
+        .once('value');
+      
       const data: any[] = [];
-      snapshot.forEach(doc => {
-        const entry = {
-          id: doc.data().ID || '',
-          name: doc.data().Name || '',
-          notes: doc.data().Notes || '',
-          status: doc.data().Status || '',
-          timeInOut: doc.data().TimeInOut || '',
-          timestamp: doc.data().Timestamp || new Date(),
-        };
-        data.push(entry);
+      snapshot.forEach((childSnapshot) => {
+        const personData = childSnapshot.val();
+        data.push({
+          idNumber: personData.idNumber || '',
+          name: personData.name || '',
+          role: personData.role || '',
+        });
       });
 
-      const latestRaEntries: { [key: string]: any } = {};
-      const displayedIds: { [key: string]: boolean } = {};
-      const filteredData: any[] = [];
-
-      // Xác định bản ghi "Ra" mới nhất cho mỗi ID
-      data.forEach(entry => {
-        if (entry.status === 'Ra') {
-          latestRaEntries[entry.id] = entry; // Lưu bản ghi "Ra" mới nhất
-        }
-      });
-
-      // Lọc các bản ghi "Vào" để chỉ hiển thị nếu có TimeInOut sau TimeInOut của bản ghi "Ra" mới nhất
-      data.forEach(entry => {
-        if (entry.status === 'Vào') {
-          const latestRa = latestRaEntries[entry.id];
-
-          // Nếu không có bản ghi "Ra", hiển thị bản ghi "Vào"
-          if (!latestRa) {
-            if (!displayedIds[entry.id]) {
-              filteredData.push(entry);
-              displayedIds[entry.id] = true; // Đánh dấu ID này đã được hiển thị
-            }
-          } else {
-            // Nếu có bản ghi "Ra", chỉ hiển thị nếu TimeInOut "Vào" lớn hơn "Ra"
-            if (entry.timeInOut > latestRa.timeInOut) {
-              if (!displayedIds[entry.id]) {
-                filteredData.push(entry);
-                displayedIds[entry.id] = true; // Đánh dấu ID này đã được hiển thị
-              }
-            }
-          }
-        }
-      });
-
-      // Không thêm bản ghi "Ra" vào dữ liệu hiển thị
-
-      setDaktnc(filteredData);
+      setDaktnc(data);
     } catch (error) {
-      console.error('Error fetching data from Firestore:', error);
+      console.error('Error fetching data from Realtime Database:', error);
     } finally {
       setLoading(false);
     }
   };
 
   useEffect(() => {
-    fetchDaktncFromFirestore();
+    fetchDaktncFromRealtimeDatabase();
 
     if (route.params?.fromHistory) {
       setActiveButton('DHDTMT17B');
@@ -82,7 +42,7 @@ const DAKTNC: React.FC<{ navigation: any, route: any }> = ({ navigation, route }
 
   const handleButtonPress = (buttonName: string) => {
     if (buttonName === activeButton) {
-      fetchDaktncFromFirestore();
+      fetchDaktncFromRealtimeDatabase();
     } else {
       setActiveButton(buttonName);
       if (buttonName === 'History') {
@@ -95,13 +55,13 @@ const DAKTNC: React.FC<{ navigation: any, route: any }> = ({ navigation, route }
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>DHDTMT17B</Text>
+      <Text style={styles.title}>DANH SÁCH SINH VIÊN ĐANG TRONG LỚP</Text>
       <ScrollView style={styles.scrollContainer}>
         <View style={styles.tableHeader}>
           <Text style={styles.tableHeaderText}>STT</Text>
           <Text style={styles.tableHeaderText}>ID</Text>
           <Text style={styles.tableHeaderText}>Name</Text>
-          <Text style={styles.tableHeaderText}>Notes</Text>
+          <Text style={styles.tableHeaderText}>Role</Text>
         </View>
         {loading ? (
           <View style={styles.loadingContainer}>
@@ -112,9 +72,9 @@ const DAKTNC: React.FC<{ navigation: any, route: any }> = ({ navigation, route }
           daktnc.map((entry, index) => (
             <View key={index} style={styles.tableRow}>
               <Text style={styles.tableCell}>{index + 1}</Text>
-              <Text style={styles.tableCell}>{entry.id}</Text>
+              <Text style={styles.tableCell}>{entry.idNumber}</Text>
               <Text style={styles.tableCell}>{entry.name}</Text>
-              <Text style={styles.tableCellLast}>{entry.notes}</Text>
+              <Text style={styles.tableCellLast}>{entry.role}</Text>
             </View>
           ))
         )}
@@ -127,21 +87,19 @@ const DAKTNC: React.FC<{ navigation: any, route: any }> = ({ navigation, route }
           <Text style={styles.buttonText}>History</Text>
         </TouchableOpacity>
       </View>
-
-      
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: { flex: 1, padding: 16, backgroundColor: '#ffffff' },
-  title: { fontSize: 20, fontWeight: 'bold', textAlign: 'center', color: 'black', marginBottom: 30 },
+  title: { fontSize: 18, fontWeight: 'bold', textAlign: 'center', color: 'black', marginBottom: 30 },
   scrollContainer: { marginTop: 20 },
   tableHeader: { flexDirection: 'row', backgroundColor: '#f1f1f1', padding: 10, borderBottomWidth: 1, borderBottomColor: '#ddd' },
   tableHeaderText: { flex: 1, fontWeight: 'bold', textAlign: 'center', color: 'black' },
   tableRow: { flexDirection: 'row', paddingVertical: 10, borderBottomWidth: 1, borderBottomColor: '#ddd' },
-  tableCell: { flex: 2, textAlign: 'center', padding: 5, color: 'black' },
-  tableCellLast: { flex: 1.5, textAlign: 'center', padding: 8, color: 'black' },
+  tableCell: { flex: 1, textAlign: 'center', padding: 5, color: 'black' },
+  tableCellLast: { flex: 1, textAlign: 'center', padding: 8, color: 'black' },
   loadingContainer: { flex: 1, justifyContent: 'center', alignItems: 'center', marginVertical: 20 },
   loadingText: { textAlign: 'center', marginVertical: 10, fontSize: 16, color: 'black' },
   buttonContainer: { flexDirection: 'row', marginTop: 20, justifyContent: 'space-between' },
--- screens/History.tsx
+++ screens/History.tsx
@@ -1,92 +1,122 @@
 import React, { useEffect, useState } from 'react';
 import { SafeAreaView, Text, ScrollView, View, TouchableOpacity, StyleSheet, ActivityIndicator } from 'react-native';
-import firestore from '@react-native-firebase/firestore';
+import database from '@react-native-firebase/database';
 
 const History: React.FC<{ navigation: any }> = ({ navigation }) => {
-  const [daktnc, setDaktnc] = useState<any[]>([]);
+  const [historyData, setHistoryData] = useState<any[]>([]);
   const [activeButton, setActiveButton] = useState<string>('History');
   const [loading, setLoading] = useState<boolean>(true);
 
-  const fetchHistoryFromFirestore = async () => {
-    setLoading(true); // Bắt đầu tải dữ liệu
-    try {
-      const snapshot = await firestore()
-        .collection('daktnc')
-        .orderBy('TimeInOut', 'asc') // Sắp xếp theo TimeInOut từ sớm đến trễ
-        .get();
-      const data: any[] = [];
+  const formatDuration = (durationInSeconds: number): string => {
+    if (durationInSeconds === 0) return ''; // Không hiển thị nếu duration là 0
 
-      snapshot.forEach(doc => {
-        const entry = {
-          id: doc.data().ID || '',
-          name: doc.data().Name || '',
-          timeInOut: doc.data().TimeInOut || '',
-          status: doc.data().Status || ''
-        };
-        data.push(entry);
+    const hours = Math.floor(durationInSeconds / 3600);
+    const minutes = Math.floor((durationInSeconds % 3600) / 60);
+    const seconds = durationInSeconds % 60;
+
+    if (hours > 0) return `${hours}h ${minutes}m`;
+    if (minutes > 0) return `${minutes}m ${seconds}s`;
+    return `${seconds}s`;
+  };
+
+  const fetchHistoryFromRealtimeDatabase = async () => {
+    setLoading(true);
+    try {
+      const snapshot = await database()
+        .ref('rooms/room1/history')
+        .once('value');
+
+      const data: any[] = [];
+      snapshot.forEach(childSnapshot => {
+        const entry = childSnapshot.val();
+        data.push({
+          time: entry.time || '',
+          idNumber: entry.idNumber || '',
+          name: entry.name || '',
+          action: entry.action || '',
+          duration: entry.duration || 0,
+        });
       });
 
-      setDaktnc(data);
+      data.sort((a, b) => {
+        const timeA = Date.parse(a.time);
+        const timeB = Date.parse(b.time);
+        return timeA - timeB;
+      });
+
+      setHistoryData(data);
     } catch (error) {
-      console.error('Error fetching history from Firestore:', error);
+      console.error('Error fetching history from Realtime Database:', error);
     } finally {
-      setLoading(false); // Đặt trạng thái tải dữ liệu thành false khi hoàn tất
+      setLoading(false);
     }
   };
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      fetchHistoryFromFirestore();
+      fetchHistoryFromRealtimeDatabase();
     }, 200);
 
-    return () => clearTimeout(timer); // Dọn dẹp timer khi component unmount
+    return () => clearTimeout(timer);
   }, []);
 
   const handleButtonPress = (buttonName: string) => {
     if (buttonName === activeButton) {
-      fetchHistoryFromFirestore(); // Tải lại dữ liệu khi nhấn nút đã chọn
+      fetchHistoryFromRealtimeDatabase();
     } else {
       setActiveButton(buttonName);
       if (buttonName === 'DHDTMT17B') {
         navigation.navigate('NHÓM 6', { fromHistory: true });
       } else {
-        console.log('Button History pressed');
-        fetchHistoryFromFirestore(); // Tải dữ liệu khi nhấn nút "HISTORY"
+        fetchHistoryFromRealtimeDatabase();
       }
     }
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>STATUS</Text>
+      <Text style={styles.title}>HISTORY</Text>
       <ScrollView style={styles.scrollContainer}>
-        <View style={styles.tableHeader}>
-          <Text style={styles.tableHeaderText}>STT</Text>
-          <Text style={styles.tableHeaderText}>ID</Text>
-          <Text style={styles.tableHeaderText}>Name</Text>
-          <Text style={styles.tableHeaderText}>Time</Text>
-          <Text style={styles.tableHeaderText}>Status</Text>
-        </View>
-        {loading ? (
-          <View style={styles.loadingContainer}>
-            <ActivityIndicator size="large" color="#007bff" />
-            <Text style={styles.loadingText}>Đang cập nhật dữ liệu...</Text>
-          </View>
-        ) : (
-          daktnc.length > 0 ? (
-            daktnc.map((entry, index) => (
-              <View key={index} style={styles.tableRow}>
-                <Text style={styles.tableCell}>{index + 1}</Text>
-                <Text style={styles.tableCell}>{entry.id}</Text>
-                <Text style={styles.tableCell}>{entry.name}</Text>
-                <Text style={styles.tableCell}>{entry.timeInOut}</Text>
-                <Text style={styles.tableCellLast}>{entry.status}</Text>
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={true}>
+          <View>
+            <View style={styles.tableHeader}>
+              <Text style={styles.tableHeaderText}>STT</Text>
+              <Text style={styles.tableHeaderText}>Time</Text>
+              <Text style={styles.tableHeaderText}>ID</Text>
+              <Text style={styles.tableHeaderText}>Name</Text>
+              <Text style={styles.tableHeaderText}>Action</Text>
+              <Text style={styles.tableHeaderText}>Duration</Text>
+            </View>
+            {loading ? (
+              <View style={styles.loadingContainer}>
+                <ActivityIndicator size="large" color="#007bff" />
+                <Text style={styles.loadingText}>Đang cập nhật dữ liệu...</Text>
               </View>
-            ))
-          ) : (
-            <Text style={styles.loadingText}>Không có dữ liệu!</Text>
-          )
-        )}
+            ) : (
+              historyData.length > 0 ? (
+                historyData.map((entry, index) => (
+                  <View key={index} style={styles.tableRow}>
+                    <Text style={styles.tableCell}>{index + 1}</Text>
+                    <Text style={styles.tableCell}>{entry.time}</Text>
+                    <Text style={styles.tableCell}>{entry.idNumber}</Text>
+                    <Text style={styles.tableCell}>{entry.name}</Text>
+                    <Text style={styles.tableCell}>{entry.action}</Text>
+                    {/* Chỉ hiển thị Duration nếu không phải là 0 */}
+                    {entry.duration > 0 ? (
+                      <Text style={styles.tableCellLast}>
+                        {formatDuration(entry.duration)}
+                      </Text>
+                    ) : (
+                      <Text style={styles.tableCellLast}>-</Text> // Hoặc hiển thị dấu gạch ngang nếu không có duration
+                    )}
+                  </View>
+                ))
+              ) : (
+                <Text style={styles.loadingText}>Không có dữ liệu!</Text>
+              )
+            )}
+          </View>
+        </ScrollView>
       </ScrollView>
       <View style={styles.buttonContainer}>
         <TouchableOpacity
@@ -110,11 +140,43 @@ const styles = StyleSheet.create({
   container: { flex: 1, padding: 16, backgroundColor: '#ffffff' },
   title: { fontSize: 20, fontWeight: 'bold', textAlign: 'center', color: 'black', marginBottom: 30 },
   scrollContainer: { marginTop: 20 },
-  tableHeader: { flexDirection: 'row', backgroundColor: '#f1f1f1', padding: 10, borderBottomWidth: 1, borderBottomColor: '#ddd' },
-  tableHeaderText: { flex: 1, fontWeight: 'bold', textAlign: 'center', color: 'black' },
-  tableRow: { flexDirection: 'row', paddingVertical: 10, borderBottomWidth: 1, borderBottomColor: '#ddd' },
-  tableCell: { flex: 2, textAlign: 'center', padding: 5, color: 'black' },
-  tableCellLast: { flex: 1.5, textAlign: 'center', padding: 8, color: 'black' },
+  tableHeader: { 
+    flexDirection: 'row', 
+    padding: 1, 
+    borderBottomWidth: 1, 
+    borderBottomColor: '#000', 
+    backgroundColor: '#f1f1f1' 
+  },
+  tableHeaderText: { 
+    flex: 1, 
+    fontWeight: 'bold', 
+    textAlign: 'center', 
+    color: 'black', 
+    borderRightWidth: 1, 
+    borderRightColor: '#000',
+    paddingVertical: 5, 
+    width: 100 
+  },
+  tableRow: { 
+    flexDirection: 'row', 
+    paddingVertical: 5, 
+    borderBottomWidth: 1, 
+    borderBottomColor: '#ddd'
+  },
+  tableCell: {  
+    textAlign: 'center', 
+    paddingVertical: 5,
+    color: 'black', 
+    borderRightWidth: 1, 
+    borderRightColor: '#ddd',
+    width: 100 
+  },
+  tableCellLast: { 
+    flex: 1, 
+    textAlign: 'center', 
+    paddingVertical: 5,
+    color: 'black' 
+  }, 
   loadingContainer: { flex: 1, justifyContent: 'center', alignItems: 'center', marginVertical: 20 },
   loadingText: { textAlign: 'center', marginVertical: 10, fontSize: 16, color: 'black' },
   buttonContainer: { flexDirection: 'row', marginTop: 20, justifyContent: 'space-between' },
